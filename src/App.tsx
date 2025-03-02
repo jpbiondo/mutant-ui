a3d03ad846eb7335e2e88f36f6f8c14c98ba5809
Juan Pablo Biondo <jupbiondo@gmail.com{>
2025-03-02 20:19:05 -0300

    ✨ feat: improve result dialog

--- src/App.tsx
+++ src/App.tsx
@@ -347,14 +347,18 @@ TCACTG"
         <dialog ref={resultModalRef} className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg">Your results just arrived!</h3>
-            <p className="py-4 text-center">
-              You are a... {""}
-              {isMutant ? (
-                <span className=" font-medium text-green-800">Mutant</span>
-              ) : (
-                <span className="font-medium text-red-800">Human</span>
-              )}
-            </p>
+            <p className="py-4 text-center text-md">You are a...</p>
+            {isMutant ? (
+              <div className="flex flex-col items-center text-green-800">
+                <DnaOff className="h-14 w-14" />
+                <span className="font-bold text-2xl">Mutant</span>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center text-red-800">
+                <DnaOff className="h-14 w-14" />
+                <span className="font-bold text-2xl">Human</span>
+              </div>
+            )}
             <div className="modal-action">
               <form method="dialog">
                 <button className="btn">Close</button>
