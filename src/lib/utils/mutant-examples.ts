2163fb6c244c6d319d76702d5210e8d492c3861c
Juan Pablo Biondo <jupbiondo@gmail.com{>
2025-03-02 19:12:01 -0300

    ✨ feat: add dna examples

---
+++ src/lib/utils/mutant-examples.ts
@@ -0,0 +1,18 @@
+export const mutantExamples = [
+  {
+    isMutant: true,
+    dna: ["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"],
+  },
+  {
+    isMutant: true,
+    dna: ["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "GCGTCA", "TCGCTG"],
+  },
+  {
+    isMutant: false,
+    dna: ["ATGCGA", "CAGTGC", "TTATTT", "AGACGG", "GCGTCA", "TCACTG"],
+  },
+  {
+    isMutant: false,
+    dna: ["ATGCTA", "CAGTGC", "TTATTT", "AGACGG", "GCGTCA", "TCACTG"],
+  },
+];
