34d0bc04533c3cb3677aacdb5eef52b53094a088
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-04 15:47:43 -0300

    🦄 refactor: remove dnastats component

--- src/components/ui/DNAStats.tsx
+++
@@ -1,9 +0,0 @@
-import { ChartData } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-
-interface DNAStatsProps {
-  data: ChartData<"doughnut", number[], unknown>;
-}
-export default function DNAStats({ data }: DNAStatsProps) {
-  return <Doughnut data={data} />;
-}
