ecdb335656cd46f9ee8c8b770db2cefb7ce311a6
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-04 15:49:18 -0300

    🦄 refactor: add new components

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,9 @@ import DNAVisualizator from "./components/ui/DNAVisualizator";
 import { stringToVectorDna, vectorToMatrixDna } from "./lib/utils/mutant.utils";
 import { Chart as ChartJS, ArcElement, Legend, Tooltip } from "chart.js";
 import { useMutant } from "./hooks/useMutant";
-import { useMutantStats } from "./hooks/useMutantStats";
-import DNAStats from "./components/ui/DNAStats";
+import MutantStats from "./components/MutantStats";
+import MutantAboutAccordion from "./components/MutantAboutAccordion";
+import MutantResultModal from "./components/MutantResultModal";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -24,7 +25,6 @@ function App() {
   const [dnaMatrix, setDnaMatrix] = useState<string[][]>([]);
   const resultModalRef = useRef<HTMLDialogElement | null>(null);
   const { isMutant, pending, error, testDna } = useMutant();
-  const { totalHumanCount, totalMutantCount, getStats } = useMutantStats();
 
   useEffect(() => {
     if (dnaInput) {
@@ -35,10 +35,6 @@ function App() {
 
     setDnaMatrix([]);
   }, [dnaInput]);
-
-  useEffect(() => {
-    getStats();
-  }, [getStats]);
 
   const openResultModal = () => {
     if (resultModalRef.current) {
@@ -65,13 +61,6 @@ function App() {
 
     openResultModal();
   };
-
-  const nucleotideItems: { name: string; color: string }[] = [
-    { name: "A - Adenine", color: "red" },
-    { name: "T - Thymine", color: "blue" },
-    { name: "C - Cytosine", color: "green" },
-    { name: "G - Guanine", color: "yellow" },
-  ];
 
   return (
     <main className="max-w-4xl mx-auto py-8">
@@ -213,115 +202,7 @@ TCACTG"
               involves detecting whether a human is a mutant based on their DNA
               sequence.
             </p>
-
-            <div className="mt-4">
-              <h3 className="text-lg font-medium text-black">How it works</h3>
-              <ul className="list-disc ml-6">
-                <li className="mt-2">
-                  A DNA sequence is represented as a string array, where each
-                  string represents a row of a matrix.
-                </li>
-                <li className="mt-2">
-                  Each letter in the strings represents a nucleotide (A, T, C,
-                  G).
-                </li>
-                <li className="mt-2">
-                  A human is considered a mutant if their DNA contains more than
-                  one sequence of four identical letters, arranged horizontally,
-                  vertically, or diagonally.
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <h3 className="text-lg font-medium text-black">Color legend</h3>
-            <ul>
-              {nucleotideItems.map(({ name, color }) => (
-                <li className="flex items-center gap-2 text-gray-700 mt-2 ml-4">
-                  <div
-                    className={`w-4 h-4 bg-${color}-100 border-${color}`}
-                  ></div>
-                  <span>{name}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="mt-4">
-            <h3 className="text-lg font-medium text-black">Examples</h3>
-            <div className="flex flex-col justify-center items-center mt-2">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap8">
-                {mutantExamples.map((example) => (
-                  <div className="flex flex-col justify-center items-center gap-4">
-                    <DNAVisualizator
-                      dnaMatrix={vectorToMatrixDna(example.dna)}
-                    />
-                    {example.isMutant ? (
-                      <div className="badge badge-error">
-                        <svg
-                          className="size-[1em]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                        >
-                          <g fill="currentColor">
-                            <rect
-                              x="1.972"
-                              y="11"
-                              width="20.056"
-                              height="2"
-                              transform="translate(-4.971 12) rotate(-45)"
-                              fill="currentColor"
-                              strokeWidth={0}
-                            ></rect>
-                            <path
-                              d="m12,23c-6.065,0-11-4.935-11-11S5.935,1,12,1s11,4.935,11,11-4.935,11-11,11Zm0-20C7.038,3,3,7.037,3,12s4.038,9,9,9,9-4.037,9-9S16.962,3,12,3Z"
-                              strokeWidth={0}
-                              fill="currentColor"
-                            ></path>
-                          </g>
-                        </svg>
-                        Human
-                      </div>
-                    ) : (
-                      <div className="badge badge-success">
-                        <svg
-                          className="size-[1em]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                        >
-                          <g
-                            fill="currentColor"
-                            strokeLinejoin="miter"
-                            strokeLinecap="butt"
-                          >
-                            <circle
-                              cx="12"
-                              cy="12"
-                              r="10"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeLinecap="square"
-                              stroke-miterlimit="10"
-                              strokeWidth="2"
-                            ></circle>
-                            <polyline
-                              points="7 13 10 16 17 8"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeLinecap="square"
-                              stroke-miterlimit="10"
-                              strokeWidth="2"
-                            ></polyline>
-                          </g>
-                        </svg>
-                        Mutant
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
+            <MutantAboutAccordion />
           </div>
         </section>
 
@@ -336,82 +217,15 @@ TCACTG"
             </p>
           </div>
 
-          <div className="mt-4 flex flex-col-reverse gap-8">
-            <div className="w-72 h-72 mx-auto">
-              <DNAStats
-                data={{
-                  labels: ["Mutant Count", "Human Count"],
-                  datasets: [
-                    {
-                      data: [totalMutantCount, totalHumanCount],
-                      backgroundColor: [
-                        "rgba(1, 102, 48, 0.2)",
-                        "rgba(159, 7, 18, 0.2)",
-                      ],
-                      borderColor: ["#016630", "#9f0712"],
-                      borderWidth: 1,
-                    },
-                  ],
-                }}
-              />
-            </div>
-            <div className=" flex-1 gap-4 text-center">
-              <div>
-                <h2 className="text-xl font-semibold">Total DNA Tests</h2>
-                <span className="text-5xl font-bold">
-                  {totalHumanCount + totalMutantCount}
-                </span>
-              </div>
-              <div className="flex flex-row gap-8 justify-center mt-6">
-                <div>
-                  <h2 className="text-lg font-semibold flex items-center gap-1">
-                    <Dna className="text-red-800" />
-                    <span className="text-red-800">Human</span>
-                    DNA Results
-                  </h2>
-                  <span className="text-3xl font-bold"> {totalHumanCount}</span>
-                </div>
-                <div>
-                  <h2 className="text-lg font-semibold flex items-center gap-1">
-                    <DnaOff className="text-green-800" />
-                    <span className="text-green-800">Mutant</span> DNA Results
-                  </h2>
-                  <span className="text-3xl font-bold">
-                    {" "}
-                    {totalMutantCount}
-                  </span>
-                </div>
-              </div>
-            </div>
-            {/* <div className="flex flex-col gap-2 items-center justify-center">
-              <Construction className="h-12 w-12" />
-              <h2 className="text-xl font-semibold">Cooming soon...</h2>
-            </div> */}
+          <div className="mt-10">
+            <MutantStats />
           </div>
         </section>
 
-        <dialog ref={resultModalRef} className="modal">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Your results just arrived!</h3>
-            <p className="py-4 text-center text-md mb-4">You are a...</p>
-            {isMutant ? (
-              <div className="flex flex-col items-center text-green-800">
-                <DnaOff className="h-14 w-14" />
-                <span className="font-bold text-2xl">Mutant</span>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center text-red-800">
-                <Dna className="h-14 w-14" />
-                <span className="font-bold text-2xl">Human</span>
-              </div>
-            )}
-            <div className="modal-action">
-              <form method="dialog">
-                <button className="btn">Close</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
+        <MutantResultModal
+          resultModalRef={resultModalRef}
+          isMutant={isMutant}
+        />
       </div>
     </main>
   );
